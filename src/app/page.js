4372ff4dc981b57f31b270f003dd00ab0941e3d2
ZukoHeiwa <166101565+JulianoMangli@users.noreply.github.com>
2024-06-22 05:54:57 +0300

    Commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar'
+import FirstSection from './pages/home/FirstSection'
+import Footer from './Footer'
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <FirstSection />
+      <Footer />
+    </main>
+  );
+}
